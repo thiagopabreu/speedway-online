0dbe13cfae873d0d8e3b62733430ede1d41c28b2
Filipe Maciel Lins <filipemlins@gmail.com>
2022-09-06 14:16:38 -0300

    add git ignore

--- deploy/PeerTest-bundle.js
+++ deploy/PeerTest-bundle.js
@@ -256,6 +256,21 @@ WL.registerComponent('drum', {
     }
 });
 
+require('./controller-teleport-component');
+require('./drum');
+require('./network-buttons');
+require('./peer-components');
+require('./Tone');
+require('./peerjs.min');
+
+require('@wonderlandengine/components/cursor');
+require('@wonderlandengine/components/cursor-target');
+require('@wonderlandengine/components/wasd-controls');
+require('@wonderlandengine/components/mouse-look');
+require('@wonderlandengine/components/hand-tracking');
+
+
+
 WL.registerComponent('network-buttons', {
     peerManagerObject: {type: WL.Type.Object},
     cursor: {type: WL.Type.Object},
